58955daeb7450de9afd102fbdcfe0c438768001b
João Medeiros <joao.gallina@einstein.br>
2024-06-28 11:21:39 -0300

    wip: projetct wip

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,23 +1,21 @@
 import { describe, expect, it } from 'vitest'
-import { Answer } from '../entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
 const fakeAnswerRepository: AnswersRepository = {
-  create: async (answer: Answer) => {
-    return;
-  }
+  create: async () => { },
 }
 describe('Create an answer', () => {
-  it('Should be able to create a new answer'), async () => {
-    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+  it('Should be able to create a new answer')
+    ; async () => {
+      const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
-    const answer = await answerQuestion.execute({
-      questionId: '1',
-      instructorId: '1',
-      content: 'teste resposta'
-    })
-
-    expect(answer.content).toEqual('teste reposta')
-  }
+      await expect(() =>
+        answerQuestion.execute({
+          questionId: '1',
+          instructorId: '1',
+          content: 'teste resposta',
+        }),
+      )
+    }
 })
