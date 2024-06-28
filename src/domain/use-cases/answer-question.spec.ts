34ebdc1488b0f3488a00b7ff1d4ab28e2d02fa66
João Medeiros <joao.gallina@einstein.br>
2024-06-27 23:41:13 -0300

    wip: use case wip

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,11 +1,18 @@
 import { describe, expect, it } from 'vitest'
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
+const fakeAnswerRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  }
+}
 describe('Create an answer', () => {
-  it('Should be able to create a new answer'), () => {
-    const answerQuestion = new AnswerQuestionUseCase()
+  it('Should be able to create a new answer'), async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
-    const answer = answerQuestion.execute({
+    const answer = await answerQuestion.execute({
       questionId: '1',
       instructorId: '1',
       content: 'teste resposta'
