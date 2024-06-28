cb15698bfe2d80c78533c7695b1798a8b4de96aa
João Medeiros <joao.gallina@einstein.br>
2024-06-27 23:41:39 -0300

    wip: question entity update

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from "crypto";
+import { Slug } from "./value-objects/slug";
 
 export interface QuestionProps {
   title: string
   content: string
+  slug: Slug;
   authorId: string
 }
 
@@ -11,11 +13,13 @@ export class Question {
   public id: string;
   public title: string;
   public content: string;
+  public slug: Slug;
   public authorId: string
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title
     this.content = props.content
+    this.slug = props.slug
     this.authorId = props.authorId
     this.id = id ?? randomUUID()
 
