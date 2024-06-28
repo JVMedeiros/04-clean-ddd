735e36d89a3f9d6fad0f6ca1e564ece84009808b
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-06-28 11:47:05 -0300

    test: answer question use case test wip

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,20 +1,17 @@
-import { describe, expect, it } from 'vitest'
+import { expect } from 'vitest'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
 const fakeAnswerRepository: AnswersRepository = {
-  create: async () => { },
+  create: async () => {},
 }
-describe('Create an answer', () => {
-  it('Should be able to create a new answer')
-    ; async () => {
-      const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
-      await expect(() =>
-        answerQuestion.execute({
-          questionId: '1',
-          instructorId: '1',
-          content: 'teste resposta',
-        }),
-      )
-    }
+test('Create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+  await expect(() =>
+    answerQuestion.execute({
+      questionId: '1',
+      instructorId: '1',
+      content: 'teste resposta',
+    }),
+  )
 })
