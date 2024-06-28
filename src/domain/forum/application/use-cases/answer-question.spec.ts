b3fd1f6186fb9ea619ade5b4a702caa15fd17e28
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-06-28 18:37:33 -0300

    refactor: answer question test refactor

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,17 +1,23 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { expect } from 'vitest'
-import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
-const fakeAnswerRepository: AnswersRepository = {
-  create: async () => {},
-}
-test('Create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
-  await expect(() =>
-    answerQuestion.execute({
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: AnswerQuestionUseCase
+
+describe('Create Question', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
+  })
+  it('Should be able to create an answer', async () => {
+    const { answer } = await sut.execute({
       questionId: '1',
       instructorId: '1',
       content: 'teste resposta',
-    }),
-  )
+    })
+
+    expect(answer.id).toBeTruthy()
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id)
+  })
 })
