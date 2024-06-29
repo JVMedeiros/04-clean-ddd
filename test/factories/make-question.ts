9aa81f7c5043aaa20bed883b9f2f86b1978dad5e
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-06-29 19:15:44 -0300

    feat: create question test factory

---
+++ test/factories/make-question.ts
@@ -0,0 +1,17 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
+import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+
+export function makeQuestion(
+  override: Partial<QuestionProps> = {}
+) {
+  const question = Question.create({
+    authorId: new UniqueEntityID(),
+    title: 'Nova pergunta',
+    content: 'Conteúdo da pergunta',
+    slug: Slug.create('example'),
+    ...override
+  })
+
+  return question
+}
