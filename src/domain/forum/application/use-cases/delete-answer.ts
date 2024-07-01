5cb84864a52a00375b7a3457d61b5dadcf8d2fe5
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-01 12:14:10 -0300

    refactor: implementing functional error handler class

--- src/domain/forum/application/use-cases/delete-answer.ts
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -1,14 +1,17 @@
+import { Either, left, right } from '@/core/either'
 import { AnswersRepository } from '../repositories/answers-repository'
+import { NotAllowedError } from './errors/not-allowed-error'
+import { ResourceNotFoundError } from './errors/resource-not-found'
 
 interface DeleteAnswerUseCaseRequest {
   authorId: string
   answerId: string
 }
 
-interface DeleteAnswerUseCaseResponse {}
+type DeleteAnswerUseCaseResponse = Either<ResourceNotFoundError | NotAllowedError, {}>
 
 export class DeleteAnswerUseCase {
-  constructor(private answersRepository: AnswersRepository) {}
+  constructor(private answersRepository: AnswersRepository) { }
   async execute({
     answerId,
     authorId,
@@ -16,14 +19,14 @@ export class DeleteAnswerUseCase {
     const answer = await this.answersRepository.findById(answerId)
 
     if (!answer) {
-      throw new Error('Answer not found')
+      return left(new ResourceNotFoundError())
     }
 
     if (authorId !== answer.authorId.toString()) {
-      throw new Error('Not allowed')
+      return left(new NotAllowedError())
     }
 
     await this.answersRepository.delete(answer)
-    return {}
+    return right({})
   }
 }
