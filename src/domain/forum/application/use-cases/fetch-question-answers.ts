12e3649ebb4d2dd1b0ab8183bc118ba086c8c011
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-01 03:38:18 -0300

    refactor: lint

--- src/domain/forum/application/use-cases/fetch-question-answers.ts
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -11,12 +11,15 @@ interface FetchQuestionAnswersUseCaseResponse {
 }
 
 export class FetchQuestionAnswersUseCase {
-  constructor(private answersRepository: AnswersRepository) { }
+  constructor(private answersRepository: AnswersRepository) {}
   async execute({
     questionId,
-    page
+    page,
   }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
-    const answers = await this.answersRepository.findManyByQuestionId(questionId, { page })
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page },
+    )
 
     return { answers }
   }
