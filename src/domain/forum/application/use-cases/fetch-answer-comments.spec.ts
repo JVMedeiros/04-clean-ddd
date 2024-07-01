8452df9dc158d3eeee3278018bd16d3ac025b0f0
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-01 03:38:08 -0300

    refactor: lint

--- src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -1,5 +1,4 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { makeAnswer } from 'test/factories/make-answer'
 import { makeAnswerComment } from 'test/factories/make-answer-comment'
 import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
 import { expect } from 'vitest'
@@ -14,19 +13,25 @@ describe('Fetch Answer Comments', () => {
     sut = new FetchAnswerCommentsUseCase(inMemoryAnswerCommentsRepository)
   })
   it('Should be able to fetch recent answer comments', async () => {
-    await inMemoryAnswerCommentsRepository.create(makeAnswerComment({
-      answerId: new UniqueEntityID('answer-1')
-    }))
-    await inMemoryAnswerCommentsRepository.create(makeAnswerComment({
-      answerId: new UniqueEntityID('answer-1')
-    }))
-    await inMemoryAnswerCommentsRepository.create(makeAnswerComment({
-      answerId: new UniqueEntityID('answer-1')
-    }))
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-1'),
+      }),
+    )
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-1'),
+      }),
+    )
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-1'),
+      }),
+    )
 
     const { answerComments } = await sut.execute({
       answerId: 'answer-1',
-      page: 1
+      page: 1,
     })
 
     expect(answerComments).toHaveLength(3)
@@ -34,14 +39,15 @@ describe('Fetch Answer Comments', () => {
 
   it('Should be able to fetch paginated answer comments', async () => {
     for (let i = 1; i <= 22; i++) {
-      await inMemoryAnswerCommentsRepository.create(makeAnswerComment({ answerId: new UniqueEntityID('answer-1') }))
+      await inMemoryAnswerCommentsRepository.create(
+        makeAnswerComment({ answerId: new UniqueEntityID('answer-1') }),
+      )
     }
     const { answerComments } = await sut.execute({
       answerId: 'answer-1',
-      page: 2
+      page: 2,
     })
 
     expect(answerComments).toHaveLength(2)
   })
 })
-
