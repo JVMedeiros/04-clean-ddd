41777b7e9345b1da240b3a3091052e5c4ffbab7b
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-01 14:40:18 -0300

    refactor: implementing functional error handler class

--- src/domain/forum/application/use-cases/comment-on-question.spec.ts
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -3,6 +3,7 @@ import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { expect } from 'vitest'
 import { CommentOnQuestionUseCase } from './comment-on-question'
+import { ResourceNotFoundError } from './errors/resource-not-found'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
@@ -27,7 +28,7 @@ describe('Comment on Question', () => {
     await sut.execute({
       questionId: question.id.toString(),
       authorId: question.authorId.toString(),
-      content: 'Comentário teste',
+      content: 'Comentário teste'
     })
 
     expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
@@ -40,12 +41,12 @@ describe('Comment on Question', () => {
 
     await inMemoryQuestionsRepository.create(question)
 
-    await expect(() => {
-      return sut.execute({
-        questionId: 'fake-id',
-        authorId: question.authorId.toString(),
-        content: 'Comentário teste',
-      })
-    }).rejects.toBeInstanceOf(Error)
+    const result = await sut.execute({
+      questionId: 'fake-id',
+      authorId: question.authorId.toString(),
+      content: 'Comentário teste',
+    })
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
   })
 })
