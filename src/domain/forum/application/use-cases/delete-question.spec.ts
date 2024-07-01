0f10402b67ad2fe4d63c06e3fa0048600b0c3dd3
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-01 03:37:46 -0300

    refactor: lint

--- src/domain/forum/application/use-cases/delete-question.spec.ts
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -13,27 +13,32 @@ describe('Delete Question', () => {
     sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
   })
   it('Should be able to delete a question', async () => {
-    const newQuestion = makeQuestion({ authorId: new UniqueEntityID('author-1') }, new UniqueEntityID('question-1'))
+    const newQuestion = makeQuestion(
+      { authorId: new UniqueEntityID('author-1') },
+      new UniqueEntityID('question-1'),
+    )
     await inMemoryQuestionsRepository.create(newQuestion)
 
     await sut.execute({
       questionId: 'question-1',
-      authorId: 'author-1'
+      authorId: 'author-1',
     })
 
     expect(inMemoryQuestionsRepository.items).toHaveLength(0)
   })
 
   it('Should not be able to delete a question from another author', async () => {
-    const newQuestion = makeQuestion({ authorId: new UniqueEntityID('author-1') }, new UniqueEntityID('question-1'))
+    const newQuestion = makeQuestion(
+      { authorId: new UniqueEntityID('author-1') },
+      new UniqueEntityID('question-1'),
+    )
     await inMemoryQuestionsRepository.create(newQuestion)
 
     await expect(() => {
       return sut.execute({
         questionId: 'question-1',
-        authorId: 'author-2'
+        authorId: 'author-2',
       })
     }).rejects.toBeInstanceOf(Error)
   })
 })
-
