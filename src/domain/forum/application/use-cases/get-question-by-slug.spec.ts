6b296957fbff524fcf7c7e3a4239b9aa34f5293d
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-01 03:38:39 -0300

    refactor: lint

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -14,16 +14,15 @@ describe('Get Question by Slug', () => {
   })
   it('Should be able to find a question by slug', async () => {
     const newQuestion = makeQuestion({
-      slug: Slug.create('example-question')
+      slug: Slug.create('example-question'),
     })
     await inMemoryQuestionsRepository.create(newQuestion)
 
     const { question } = await sut.execute({
-      slug: 'example-question'
+      slug: 'example-question',
     })
 
     expect(question.id).toBeTruthy()
     expect(question.title).toEqual(newQuestion.title)
   })
 })
-
