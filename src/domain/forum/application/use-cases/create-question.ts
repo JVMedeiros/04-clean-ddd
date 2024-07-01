61f280243fd573c45abec182134124dff8664e2b
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-01 12:14:02 -0300

    refactor: implementing functional error handler class

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -1,3 +1,4 @@
+import { Either, right } from '@/core/either'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-respository'
@@ -8,12 +9,12 @@ interface CreateQuestionUseCaseRequest {
   content: string
 }
 
-interface CreateQuestionUseCaseResponse {
+type CreateQuestionUseCaseResponse = Either<null, {
   question: Question
-}
+}>
 
 export class CreateQuestionUseCase {
-  constructor(private questionsRepository: QuestionsRepository) {}
+  constructor(private questionsRepository: QuestionsRepository) { }
   async execute({
     authorId,
     title,
@@ -26,6 +27,6 @@ export class CreateQuestionUseCase {
     })
 
     await this.questionsRepository.create(question)
-    return { question }
+    return right({ question })
   }
 }
