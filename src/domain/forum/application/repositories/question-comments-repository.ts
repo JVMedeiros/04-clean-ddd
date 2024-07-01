dad1426c5d82ed3a2be87c96647802f02377d1fc
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-01 03:36:49 -0300

    refactor: lint

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -5,5 +5,8 @@ export interface QuestionCommentsRepository {
   create(questionComment: QuestionComment): Promise<void>
   delete(questionComment: QuestionComment): Promise<void>
   findById(id: string): Promise<QuestionComment | null>
-  findManyByQuestionId(questionId: string, params: PaginationParams): Promise<QuestionComment[]>
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<QuestionComment[]>
 }
