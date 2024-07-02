ff64faebeb5d3a6e9cd537077c735f42b8323708
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-02 00:15:46 -0300

    refactor: lint

--- src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -21,7 +21,7 @@ describe('Delete Answer Comment', () => {
 
     await sut.execute({
       answerCommentId: answerComment.id.toString(),
-      authorId: answerComment.authorId.toString()
+      authorId: answerComment.authorId.toString(),
     })
 
     expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0)
@@ -39,7 +39,6 @@ describe('Delete Answer Comment', () => {
 
     expect(result.isLeft()).toBe(true)
     expect(result.value).toBeInstanceOf(ResourceNotFoundError)
-
   })
 
   it('Should not be able to delete another user answer comment', async () => {
@@ -55,6 +54,5 @@ describe('Delete Answer Comment', () => {
     })
     expect(result.isLeft()).toBe(true)
     expect(result.value).toBeInstanceOf(NotAllowedError)
-
   })
 })
