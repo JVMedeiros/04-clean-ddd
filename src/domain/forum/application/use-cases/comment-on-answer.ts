9d9c2c786b9d420bece50de3ecc60a2ea30954e1
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-02 22:18:08 -0300

    refactor: folder path adjust

--- src/domain/forum/application/use-cases/comment-on-answer.ts
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -3,7 +3,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
 import { AnswersRepository } from '../repositories/answers-repository'
-import { ResourceNotFoundError } from './errors/resource-not-found'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found'
 
 interface CommentOnAnswerUseCaseRequest {
   authorId: string
@@ -22,7 +22,7 @@ export class CommentOnAnswerUseCase {
   constructor(
     private answersRepository: AnswersRepository,
     private answerCommentsRepository: AnswerCommentsRepository,
-  ) {}
+  ) { }
 
   async execute({
     authorId,
