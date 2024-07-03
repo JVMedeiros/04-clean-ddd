7b51cfe987384acca1b7b8f2bffa866879dc1a5d
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-03 16:09:43 -0300

    refactor: folder path adjust

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,8 +1,8 @@
 import { Either, right } from '@/core/either'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { Answer } from '../../enterprise/entities/answer'
-import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
-import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
+import { Answer } from '../enterprise/entities/answer'
+import { AnswerAttachment } from '../enterprise/entities/answer-attachment'
+import { AnswerAttachmentList } from '../enterprise/entities/answer-attachment-list'
 import { AnswersRepository } from '../repositories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
