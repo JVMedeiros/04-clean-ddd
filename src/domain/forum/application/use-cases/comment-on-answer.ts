87a51ed577cc828667bb4f57194354c38f8b345f
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-03 16:09:55 -0300

    refactor: folder path adjust

--- src/domain/forum/application/use-cases/comment-on-answer.ts
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -1,6 +1,6 @@
 import { Either, left, right } from '@/core/either'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { AnswerComment } from '../enterprise/entities/answer-comment'
 import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
 import { AnswersRepository } from '../repositories/answers-repository'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found'
