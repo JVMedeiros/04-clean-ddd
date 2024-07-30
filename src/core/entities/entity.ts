74790d00b0a408544262f0e271947a2038ae6665
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-30 11:35:52 -0300

    fix: eslint fix

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -13,7 +13,7 @@ export class Entity<Props> {
     this._id = id ?? new UniqueEntityID(id)
   }
 
-  public equals(entity: Entity<any>) {
+  public equals(entity: Entity<unknown>) {
     if (entity === this) {
       return true
     }
